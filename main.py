af510be81ddc72f3cfcf257086000b7f94cf8e9e
FatihErenD <fatiherendurmus@hotmail.com>
2024-12-11 21:42:41 +0300

    UIs added

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUi
 
+from UI import mainDoctor
 
-app = QApplication(sys.argv)
-widget = loadUi('UI\\mainDoctor.ui')
-widget.show()
-sys.exit(app.exec_())
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = mainDoctor.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+App = QApplication([])
+window = Main()
+window.show()
+App.exec()
