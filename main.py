3dad7dd9842b3fd88b7f5d96de74cc04e802d122
zaid <zaidalmoughrabi@gmail.com>
2024-12-16 20:08:53 +0300

    Redesigned file structure

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def logOut(screen):
     screen.hide()
     window.show()
 
+import os
+os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = r"D:\Ders Notları\Veritabanı Yönetim Sistemleri\Project\HFZ_Hospital\.venv\Lib\site-packages\PyQt5\Qt5\plugins"
 
 
 class Main(QMainWindow):
